60a4cf9954664e995f93ab5221ad5f1526b3feee
Dominic Maas <dominic.maas@live.com>
2019-08-13 09:26:22 +1200

    Mobile CSS layout, copy other pages

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -15,13 +15,11 @@ namespace Website
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,7 +29,6 @@ namespace Website
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
