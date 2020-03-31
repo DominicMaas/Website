8f784ce8c3d9a8b95a8ca02404324a8f67240a0c
Dominic Maas <dominic.maas@live.com>
2020-03-31 22:33:05 +1300

    Some more misc work (cleaning up style and old code)

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -2,18 +2,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Website.Common;
 
 namespace Website
 {
     public class Startup
     {
+        private IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages(options =>
+            var builder = services.AddRazorPages(options =>
             {
                 options.Conventions.Add(new PageRouteTransformerConvention(new SlugifyParameterTransformer()));
             });
+
+#if DEBUG
+            if (Environment.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
+#endif
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -21,7 +36,6 @@ namespace Website
             if (env.EnvironmentName == "Development")
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
