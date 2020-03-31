b0601aac85a934eaf58bdbec5e843cb61b522dc1
Dominic Maas <dominic.maas@live.com>
2020-03-31 20:48:02 +1300

    Update packages, switch to 3.1, add min files

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Website.Common;
 
@@ -9,22 +8,12 @@ namespace Website
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages(options =>
             {
                 options.Conventions.Add(new PageRouteTransformerConvention(new SlugifyParameterTransformer()));
             });
-
-            // Google Analytics
-            // services.AddSingleton<ITagHelperComponent>(new GoogleAnalyticsTagHelperComponent("UA-37972059-9"));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
