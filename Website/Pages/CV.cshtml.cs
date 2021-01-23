944111d7508615e3880b8981f1edf99499bddb41
Dominic Maas <dominic.maas@live.com>
2021-01-24 09:25:59 +1300

    Update style

--- Website/Pages/CV.cshtml.cs
+++ Website/Pages/CV.cshtml.cs
@@ -4,8 +4,13 @@ namespace Website.Pages
 {
     public class ExperienceModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(bool show = false)
         {
+            ViewData["IsLookingForWork"] = false;
+            
+            // Just override this variable
+            if (show == true)
+                ViewData["IsLookingForWork"] = true;
         }
     }
 }
