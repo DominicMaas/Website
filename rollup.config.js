f4939043a16d78d2de2a3606fac9204670f39f01
Dominic Maas <dominic.maas@live.com>
2021-02-20 11:21:26 +1300

    Make sure the target folder is cleaned on build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import rust from '@wasm-tool/rollup-plugin-rust'
+import del from 'rollup-plugin-delete'
 
 export default {
     input: {
         testing: 'wasm/testing/Cargo.toml',
     },
     plugins: [
+        del({ targets: 'web/wwwroot/wasm/*' }),
         rust({
             serverPath: '/wasm/'
         }),
