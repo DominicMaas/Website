6bbcd14cf7423c331970950695a89ada4f41a629
Dominic Maas <dominic.maas@live.com>
2021-02-25 18:00:06 +1300

    A bit of cleaning up

--- web/Startup.cs
+++ web/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Website.Common;
 
