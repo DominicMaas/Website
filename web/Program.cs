3fca490da4cfa9c3bbdfb4818e18ddfe4ee1b8e6
Dominic Maas <dominic.maas@live.com>
2022-11-24 21:39:41 +1300

    wow this actually works now! Fix the embedded gist not working

--- web/Program.cs
+++ web/Program.cs
@@ -69,7 +69,7 @@ app.MapControllers();
 app.Use(async (context, next) =>
 {
     // Security Headers
-    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' ajax.cloudflare.com static.cloudflareinsights.com");
+    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' ajax.cloudflare.com static.cloudflareinsights.com gist.github.com; style-src 'self' 'unsafe-inline' github.githubassets.com;");
     context.Response.Headers.Add("X-Frame-Options", "DENY");
     context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
