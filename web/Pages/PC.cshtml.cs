b232db7589136a390d7161af950f6c1effe1064b
Dominic Maas <dominic.maas@live.com>
2023-03-14 22:24:46 +1300

    more misc stuff

---
+++ web/Pages/PC.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class PCModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
