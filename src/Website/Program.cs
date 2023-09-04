2e593dd6bcec8431c93758c9449124bf846af7aa
Dominic Maas <dominic.maas@live.com>
2023-09-05 08:42:08 +1200

    maybe fix the api that has been broken for a while...

--- src/Website/Program.cs
+++ src/Website/Program.cs
@@ -56,6 +56,17 @@ if (environment.IsDevelopment())
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    // Security Headers
+    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' ajax.cloudflare.com static.cloudflareinsights.com gist.github.com; style-src 'self' 'unsafe-inline' github.githubassets.com; frame-src 'self' www.youtube-nocookie.com; img-src 'self' i.ytimg.com;");
+    context.Response.Headers.Add("X-Frame-Options", "DENY");
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+
+    await next();
+});
+
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
@@ -81,17 +92,6 @@ app.UseRouting();
 app.MapRazorPages();
 
 app.MapControllers();
-
-app.Use(async (context, next) =>
-{
-    // Security Headers
-    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' ajax.cloudflare.com static.cloudflareinsights.com gist.github.com; style-src 'self' 'unsafe-inline' github.githubassets.com; frame-src 'self' www.youtube-nocookie.com; img-src 'self' i.ytimg.com;");
-    context.Response.Headers.Add("X-Frame-Options", "DENY");
-    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-    context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
-
-    await next();
-});
 
 
 // Run our migrations on start up
